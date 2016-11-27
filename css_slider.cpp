fdc56828bdff0ac726c05d86810cdaed194f47f1
Violline <ola.mankiewicz@gmail.com>
2016-11-27 15:45:59 +0100

    kod do slidera, nie do konca cool

---
+++ css_slider.cpp
@@ -0,0 +1,66 @@
+this->setStyleSheet(
+                    "#buttonsStyle {"
+                    "font: bold 14px;"
+                    "border: none;  }"
+
+                    "QMainWindow {"
+                    "color: qlineargradient(spread:pad, x1:0 y1:0, x2:1 y2:01 stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255));"
+                    "background: qlineargradient( x1:0 y1:0, x2:1 y2:1, stop:0 gray, stop:1 white);}"
+
+                    "QGroupBox {"
+                    "border: 0 }"
+
+                    "#mainStyle {"
+                    "background-image: url(C:/Users/Olka/Documents/Studia 4/ENG THESIS/icons/x.png); "
+                    "background-position: bottom; "
+                    "background-repeat: no-repeat;"
+                    "background-size: cover; }"
+
+                    "QSlider::groove:vertical {"
+                    "border: 1px solid #bbb;"
+                    "background: white;"
+                    "width: 15px;"
+                    "border-radius: 4px; }"
+
+                    "QSlider::sub-page:vertical {"
+                    "background: qlineargradient(x1: 0, y1: 1,    x2: 0, y2: 0,stop: 0 #66e, stop: 1 #bbf);"
+                    "background: qlineargradient(x1: 0, y1: 0.2, x2: 1, y2: 1,stop: 0 #bbf, stop: 1 #55f);"
+                    "border: 1px solid #777;"
+                    "width: 15px;"
+                    "border-radius: 4px;}"
+
+                    "QSlider::add-page:vertical {"
+                    "background: #fff;"
+                    "border: 1px solid #777;"
+                    "width: 15px;"
+                    "border-radius: 4px; }"
+
+
+                    "QSlider::handle:vertical {"
+                    "background: qlineargradient(x1:0, y1:0, x2:1, y2:1,stop:0 #eee, stop:1 #ccc);"
+                    "border: 1px solid #777;"
+
+                    "width: 20px;"
+                    "margin-top: -2px;"
+                    "margin-bottom: -2px;"
+                    "border-radius: 4px;}"
+
+                    "QSlider::handle:vertical:hover {"
+                    "background: qlineargradient(x1:0, y1:0, x2:1, y2:1,stop:0 #fff, stop:1 #ddd);"
+                    "border: 1px solid #444;"
+                    "border-radius: 4px;}"
+
+                    "QSlider::sub-page:vertical:disabled {"
+                    "background: #bbb;"
+                    "border-color: #999;}"
+
+                    "QSlider::add-page:vertical:disabled {"
+                    "background: #eee;"
+                    "border-color: #999;}"
+
+                    "QSlider::handle:vertical:disabled {"
+                    "background: #eee;"
+                    "border: 1px solid #aaa;"
+                    "border-radius: 4px;}"
+
+                    );
